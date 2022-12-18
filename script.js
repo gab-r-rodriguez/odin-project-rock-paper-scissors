ac37aa3f8e9e510ddb96286d62a7f56493fab8ec
Gabriel Rodriguez <gabrielroquerodriguez@gmail.com>
2022-12-18 14:35:13 -0800

    create index and script files. add pseudocode, first functions, and variables.

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+pseudocode
+
+Done
+1. Get rock/paper/scissors input choice from player
+
+To do
+2. Get rock/paper/scissors choice from computer
+3. Compare 1 and 2 with conditions. Declare a winner
+4. Return value declaring who the winner is
+*/
+
+const playerSelection = "rock";
+const gameChoices = ['rock','paper','scissors']
+const computerSelection = getComputerChoice();
+
+
+function getComputerChoice() {
+// randomly return rock paper scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+// return a string that declares a winner
+// make sure this is case insensitive    
+}
