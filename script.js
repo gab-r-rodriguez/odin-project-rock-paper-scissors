fa67873ce74890983db7f1a0749566352dd0496a
Gabriel Rodriguez <gabrielroquerodriguez@gmail.com>
2022-12-19 19:13:27 -0800

    Fix 'playRound' function and draft a 'gameæ function. Also start prompting user for input

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-/*
-pseudocode
+// declare variables for player's choice and computer's choice
+let playerSelection = prompt('rock, paper, or scissors?');
+let playerChoice = playerSelection.toLowerCase(); // account for case sensitive inputs
+const gameChoices = ['rock', 'paper', 'scissors'];
+let computerSelection = getComputerChoice();
 
-Done
-1. Get rock/paper/scissors input choice from player
+// record a score for each round played
+let playerScore = 0;
+let computerScore = 0;
 
-To do
-2. Get rock/paper/scissors choice from computer
-3. Compare 1 and 2 with conditions. Declare a winner
-4. Return value declaring who the winner is
-*/
-
-const playerSelection = "rock";
-const gameChoices = ['rock','paper','scissors']
-const computerSelection = getComputerChoice();
-
-
+// randomly select a game choice for the computer
 function getComputerChoice() {
-// randomly return rock paper scissors
+    const computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
 // return a string that declares a winner
-// make sure this is case insensitive    
+function playRound(playerChoice, computerSelection) {
+    let roundResult = '';
+
+    if ((playerChoice == 'rock' && computerSelection == 'scissors') ||
+        (playerChoice == 'scissors' && computerSelection == 'paper') ||
+        (playerChoice == 'paper' && computerSelection == 'rock')) {
+        roundResult = 'You win';
+        playerScore = playerScore + 1;
+    }
+
+    else if ((playerChoice == 'rock' && computerSelection == 'paper') ||
+        (playerChoice == 'scissors' && computerSelection == 'rock') ||
+        (playerChoice == 'paper' && computerSelection == 'scissors')) {
+        roundResult = 'You lose';
+        computerScore = computerScore + 1;
+    }
+
+    else
+        roundResult = 'It was a draw'
+
+    return roundResult, playerScore;
+    return computerScore;
+}
+
+function game() {
+    // loop playing game until 5 rounds ends
+    for (let i = 1; i < 6; i++) {
+        playRound(playerSelection, computerSelection);
+    }
+
+    // keep a score
+    return 
+
 }
